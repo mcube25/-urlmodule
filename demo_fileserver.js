4e97b280127beb72a65519bee3f7971320708c21
mcube25 <mcube3567@gmail.com>
2021-03-09 23:34:49 -0800

    post node

--- demo_fileserver.js
+++ demo_fileserver.js
@@ -77,6 +77,13 @@ http.createServer(function(req, res){
        if (err) throw err;
        res.write('File uploaded and moved!');
        res.end();
-    })
+    });
+  }else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
   }
 })
