5fa1cb0a031092f039e63835d2ddbb871b0b709e
mcube25 <mcube3567@gmail.com>
2021-03-22 16:16:33 -0700

    mysql database connection

--- mysql.js
+++ mysql.js
@@ -2,3 +2,12 @@
 //node.js can be used in database application
 //download mysql
 //install mysql driver
+//npm install mysql
+var mysql = require('mysql');
+//create a connection
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host:"localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
