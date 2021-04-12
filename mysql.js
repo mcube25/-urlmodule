99f285c8e3ffdf5945f06315f4baab084fbdcfc3
mcube25 <mcube3567@gmail.com>
2021-04-12 00:45:45 -0700

    creating primary key

--- mysql.js
+++ mysql.js
@@ -44,3 +44,22 @@ con.connect(function(err) {
     console.log("Table created");
   });
 });
+//primary key
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
