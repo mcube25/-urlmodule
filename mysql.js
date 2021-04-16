7aed5d3a458eda27c9b326e5d368bc45e0e9697f
mcube25 <mcube3567@gmail.com>
2021-04-15 22:07:23 -0700

    adding private key 2

--- mysql.js
+++ mysql.js
@@ -73,3 +73,12 @@ var con = mysql.createConnection({
   password: "yourpassword",
   database: "mydb"
 });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table altered");
+  });
+});
