06533411dc80fa43856d149d08ad3e43844cef10
mcube25 <mcube3567@gmail.com>
2021-04-18 00:29:42 -0700

    insert into statement

--- mysql.js
+++ mysql.js
@@ -84,3 +84,16 @@ con.connect(function(err) {
 });
 //to fill tables in MySql use the "INSERT INTO" statement
 //example insert a record in the customers table
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+ user: "yourusername",
+ password: "yourpassword",
+ database: "mydb"
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+con.query(sql, function (err, result) {
+  
